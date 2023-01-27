b6f8499109e0e853094f77c95cdb129063d1a5fe
donghyun <ilovekdh1208@ajou.ac.kr>
2023-01-27 12:16:47 +0900

    first commit

---
+++ src/main/java/com/IssueCalendar/IssueCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.IssueCalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IssueCalendar/IssueCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IssueCalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
